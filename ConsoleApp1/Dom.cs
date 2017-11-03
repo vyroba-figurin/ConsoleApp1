2130cee97bc4341b112fb944bd2984259822154a
CSharpFri <CSharpFri@CLASS-1-1>
2017-11-03 18:50:22 +0100

    feat(OOP) Dom class added

---
+++ ConsoleApp1/Dom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Dom
+    {
+        private string nazov, typ, poschodie;
+        private int obytneMiestnosti;
+        private float uzitkovaPlocha, zastavanaPlocha;
+
+        public Dom(string nazov, string typ, string poschodie, int obytneMiestnosti, float uzitkovaPlocha, float zastavanaPlocha)
+        {
+            this.nazov = nazov;
+            this.typ = typ;
+            this.poschodie = poschodie;
+            this.obytneMiestnosti = obytneMiestnosti;
+            this.uzitkovaPlocha = uzitkovaPlocha;
+            this.zastavanaPlocha = zastavanaPlocha;
+        }
+    }
+}
